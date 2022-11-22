065811bcda86e1b4108083c20fac237d486972ca
levy kipkoech <kipkoechlevy096@gmail.com>
2022-11-22 08:48:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let length= names.length;
+
+
+function writeCards(names, event) {
+    for (let i = 0;i < length; i++) { 
+        console.log(`Thank you, ${names[i]}, for the ${event} gift!`);
+    }
+        return(`Thank you, ${names}, for the ${event} gift!`);
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown() {
+    var countDown = 10;
+    while (countDown > 0) {
+    console.log(countDown--);
+    }
+    return(countDown--);
+}
+console.log(countDown(10));
+
+function countDowns() {
+    var countDowns= 4;
+    while (countDowns > 0) {
+        console.log(countDowns--);
+    }
+    return(countDowns--);
+}
+console.log(countDowns(4));
+
